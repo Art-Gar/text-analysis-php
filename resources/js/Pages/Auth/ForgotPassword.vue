6ed0bbf0313ad402c1ca480f3f61b146228ef8f2
art-gar <72654944+Art-Gar@users.noreply.github.com>
2023-12-26 22:47:32 +0200

    translation

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -26,8 +26,7 @@ const submit = () => {
         <Head title="Forgot Password" />
 
         <div class="mb-4 text-sm text-gray-600">
-            Forgot your password? No problem. Just let us know your email address and we will email you a password reset
-            link that will allow you to choose a new one.
+            Jei užmiršote slaptažodį įrašykite savo elektroninį paštą, su kuriuo jūsų paskyra registruota. Kai paspausiite, į paštą bus išsiųsta slaptažodžio pakeitimo nuoroda.
         </div>
 
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
@@ -36,7 +35,7 @@ const submit = () => {
 
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="email" value="Email" />
+                <InputLabel for="email" value="El. paštas" />
 
                 <TextInput
                     id="email"
@@ -53,7 +52,7 @@ const submit = () => {
 
             <div class="flex items-center justify-end mt-4">
                 <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Email Password Reset Link
+                    Siųsti nuorodą.
                 </PrimaryButton>
             </div>
         </form>
